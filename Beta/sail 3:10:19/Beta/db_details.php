fa4f6597972d7efb5e552808827cd05df3ad8ae6
Sailaja Kuchibhatla <sk2292@njit.edu>
2019-03-11 09:28:40 -0400

    latest update

---
+++ Beta/sail 3:10:19/Beta/db_details.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "sql1.njit.edu";
+$username = "sk2292";
+$password = "SMkANvWl";
+$project = "sk2292";
+?>
---
+++ Beta/sail 3:10:19/Beta/show_tests.php
@@ -0,0 +1,36 @@
+<?php
+
+    $status = isset($_POST['Status']) ? $_POST['Status'] : '';
+    
+
+    //posting with curl
+
+    $url = 'https://web.njit.edu/~sk2292/Beta/show_tests_middle.php';
+    $post_data = array(
+        'Status' => $status,
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url); // url to send to
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return output instead of printing
+    curl_setopt($ch, CURLOPT_POST, 1); //posting
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); //add post variables to request
+    $output = curl_exec($ch); //execute request and fetch response
+    if ($output == FALSE){ //check if request successful
+        echo "1cURL error: " . curl_error($ch);
+    }
+    curl_close($ch); //close curl
+
+
+    if($output != "no data"){
+        print_r($output);
+        echo "<p>Enter the Test ID of the test you want to open</p><br>";
+        echo '<input type="text" placeholder="Test ID" id="TestIDGetTest"/>';
+        echo '<input type="button" value="Open Test" onclick="openTest()">';
+
+    }
+
+?>
+
+
+
