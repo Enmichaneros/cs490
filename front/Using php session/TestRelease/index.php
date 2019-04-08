eff0515dcf302f15d0582d1ee13940e9ab45348c
Krystal <jj373@njit.edu>
2019-04-08 19:52:40 -0400

    changed styles

--- front/Using php session/TestRelease/index.php
+++ front/Using php session/TestRelease/index.php
@@ -13,18 +13,19 @@
     <main>
         <div class="wrapper-main">
             <section class="section-default">
-                <h1>Sign In</h1>
-                <?php
-                    if (isset($_GET['error'])) {
-                        if ($_GET['error'] == "emptyfields"){
-                            echo '<p class="loginerror">Fill in all the fields!</p>';
-                        }
-                        else if ($_GET['error'] == "invalidlogin"){
-                            echo '<p class="loginerror">Check your credentials!</p>';
-                        }
-                    }
-                ?>
+                
                 <form class="signup-form" action="includes/login.inc.php" method="post">
+                    <h2>Sign In</h2>
+                    <?php
+                        if (isset($_GET['error'])) {
+                            if ($_GET['error'] == "emptyfields"){
+                                echo '<p class="loginerror">Fill in all the fields!</p>';
+                            }
+                            else if ($_GET['error'] == "invalidlogin"){
+                                echo '<p class="loginerror">Check your credentials!</p>';
+                            }
+                        }
+                    ?>
                     <input type="text" name="UCID" placeholder="UCID">
                     <input type="password" name="Pass" placeholder="PASSWORD">
                     <button type="submit" name="submit">Login</button>  
