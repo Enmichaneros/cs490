55f5c604abbb97e521eae94440ae1035b5d174f8
Krystal <jj373@njit.edu>
2019-05-05 21:03:52 -0400

    merge with sail changes

---
+++ RV/middle/add_test_questions_middle.php
@@ -0,0 +1,31 @@
+<?php
+    $testname = $_POST['TestName'];
+    $qid = $_POST['QID'];
+    $points = $_POST['Points'];
+    $totalpoints = $_POST['TotalPoints'];
+
+
+    //posting with curl to SQL
+
+    $url = 'https://web.njit.edu/~sk2292/RV/add_test_questions_db.php';
+    $post_data = array(
+        'TestName' => $testname,
+        'QID' => $qid,
+        'Points' => $points,
+        'TotalPoints' => $totalpoints,
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url); // url to send to
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return output instead of printing
+    curl_setopt($ch, CURLOPT_POST, 1); //posting
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); //add post variables to request
+
+    $output = curl_exec($ch); //execute request and fetch response
+    if ($output == FALSE){ //check if request successful
+        echo "2cURL error: " . curl_error($ch);
+    }
+    curl_close($ch); //close curl
+
+    echo $output;
+
+?>
---
+++ RV/middle/release_grades_middle.php
@@ -0,0 +1,25 @@
+<?php
+    $testid = $_POST['TestID'];
+
+
+    //posting with curl to SQL
+
+    $url = 'https://web.njit.edu/~sk2292/RV/release_grades_db.php';
+    $post_data = array(
+        'TestID' => $testid,
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url); // url to send to
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return output instead of printing
+    curl_setopt($ch, CURLOPT_POST, 1); //posting
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); //add post variables to request
+
+    $output = curl_exec($ch); //execute request and fetch response
+    if ($output == FALSE){ //check if request successful
+        echo "cURL error: " . curl_error($ch);
+    }
+    curl_close($ch); //close curl
+
+    echo $output;
+
+?>
